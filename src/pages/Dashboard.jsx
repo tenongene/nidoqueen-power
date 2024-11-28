e3c38dbd83558055d6557cb1033132e4b0bb0309
Tenongene <tenongene00@gmail.com>
2024-11-28 09:48:52 -0600

    Sidebar and header cleaned up

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -20,89 +20,79 @@ import DashboardCard13 from '../partials/dashboard/DashboardCard13';
 import Banner from '../partials/Banner';
 
 function Dashboard() {
+	const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+	return (
+		<div className="flex h-screen overflow-hidden">
+			{/* Sidebar */}
+			<Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-  return (
-    <div className="flex h-screen overflow-hidden">
+			{/* Content area */}
+			<div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+				{/*  Site header */}
+				<Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-      {/* Sidebar */}
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+				<main className="grow">
+					<div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+						{/* Dashboard actions */}
+						<div className="sm:flex sm:justify-between sm:items-center mb-8">
+							{/* Left: Title */}
+							<div className="mb-4 sm:mb-0">
+								<h1 className="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">Dashboard</h1>
+							</div>
 
-      {/* Content area */}
-      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+							{/* Right: Actions */}
+							<div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+								{/* Filter button */}
+								{/* <FilterButton align="right" /> */}
+								{/* Datepicker built with flatpickr */}
+								<Datepicker align="right" />
+								{/* Add view button */}
+								{/* <button className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white">
+									<svg className="fill-current shrink-0 xs:hidden" width="16" height="16" viewBox="0 0 16 16">
+										<path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+									</svg>
+									<span className="max-xs:sr-only">Add View</span>
+								</button> */}
+							</div>
+						</div>
 
-        {/*  Site header */}
-        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+						{/* Cards */}
+						<div className="grid grid-cols-12 gap-6">
+							{/* Line chart (Acme Plus) */}
+							<DashboardCard01 />
+							{/* Line chart (Acme Advanced) */}
+							<DashboardCard02 />
+							{/* Line chart (Acme Professional) */}
+							<DashboardCard03 />
+							{/* Bar chart (Direct vs Indirect) */}
+							<DashboardCard04 />
+							{/* Line chart (Real Time Value) */}
+							<DashboardCard05 />
+							{/* Doughnut chart (Top Countries) */}
+							<DashboardCard06 />
+							{/* Table (Top Channels) */}
+							<DashboardCard07 />
+							{/* Line chart (Sales Over Time) */}
+							<DashboardCard08 />
+							{/* Stacked bar chart (Sales VS Refunds) */}
+							<DashboardCard09 />
+							{/* Card (Customers) */}
+							<DashboardCard10 />
+							{/* Card (Reasons for Refunds) */}
+							<DashboardCard11 />
+							{/* Card (Recent Activity) */}
+							<DashboardCard12 />
+							{/* Card (Income/Expenses) */}
+							<DashboardCard13 />
+						</div>
+					</div>
+				</main>
 
-        <main className="grow">
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-
-            {/* Dashboard actions */}
-            <div className="sm:flex sm:justify-between sm:items-center mb-8">
-
-              {/* Left: Title */}
-              <div className="mb-4 sm:mb-0">
-                <h1 className="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">Dashboard</h1>
-              </div>
-
-              {/* Right: Actions */}
-              <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-                {/* Filter button */}
-                <FilterButton align="right" />
-                {/* Datepicker built with flatpickr */}
-                <Datepicker align="right" />
-                {/* Add view button */}
-                <button className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white">
-                  <svg className="fill-current shrink-0 xs:hidden" width="16" height="16" viewBox="0 0 16 16">
-                    <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                  </svg>
-                  <span className="max-xs:sr-only">Add View</span>
-                </button>                
-              </div>
-
-            </div>
-
-            {/* Cards */}
-            <div className="grid grid-cols-12 gap-6">
-
-              {/* Line chart (Acme Plus) */}
-              <DashboardCard01 />
-              {/* Line chart (Acme Advanced) */}
-              <DashboardCard02 />
-              {/* Line chart (Acme Professional) */}
-              <DashboardCard03 />
-              {/* Bar chart (Direct vs Indirect) */}
-              <DashboardCard04 />
-              {/* Line chart (Real Time Value) */}
-              <DashboardCard05 />
-              {/* Doughnut chart (Top Countries) */}
-              <DashboardCard06 />
-              {/* Table (Top Channels) */}
-              <DashboardCard07 />
-              {/* Line chart (Sales Over Time) */}
-              <DashboardCard08 />
-              {/* Stacked bar chart (Sales VS Refunds) */}
-              <DashboardCard09 />
-              {/* Card (Customers) */}
-              <DashboardCard10 />
-              {/* Card (Reasons for Refunds) */}
-              <DashboardCard11 />
-              {/* Card (Recent Activity) */}
-              <DashboardCard12 />
-              {/* Card (Income/Expenses) */}
-              <DashboardCard13 />
-              
-            </div>
-
-          </div>
-        </main>
-
-        <Banner />
-
-      </div>
-    </div>
-  );
+				{/* <Banner /> */}
+			</div>
+		</div>
+	);
 }
 
 export default Dashboard;
